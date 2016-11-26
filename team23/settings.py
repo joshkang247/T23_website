4b4e1317c75225ffd420385e0654a53001b2e460
Matthew Black <mblackmblackmblack@gmail.com>
2016-11-25 23:28:56 -0500

    basic setup"

--- team23/settings.py
+++ team23/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'team23.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/mblack/Django/team23/team23/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
